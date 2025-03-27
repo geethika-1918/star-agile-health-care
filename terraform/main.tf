5ddb0441331882a11ecca1a8d34eebdb508043ee
geethika-1918 <100533888+geethika-1918@users.noreply.github.com>
2025-03-27 20:09:34 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,7 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
 resource "aws_instance" "kubernetes_master" {
-  ami           = "ami-0c55b159cbfafe1f0"
+  ami           = "ami-0e35ddab05955cf57"
   instance_type = "t2.medium"
-  key_name      = "your-key"
+  key_name      = "ubuntu-machine"
   
   tags = {
     Name = "Kubernetes-Master"
@@ -14,9 +10,9 @@ resource "aws_instance" "kubernetes_master" {
 
 resource "aws_instance" "kubernetes_worker" {
   count         = 2
-  ami           = "ami-0c55b159cbfafe1f0"
+  ami           = "ami-0e35ddab05955cf57"
   instance_type = "t2.medium"
-  key_name      = "your-key"
+  key_name      = "ubuntu-machine"
   
   tags = {
     Name = "Kubernetes-Worker-${count.index}"
