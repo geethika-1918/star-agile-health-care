6e6b83042f7d0c3c301593edd7ad4b080081ad06
geethika-1918 <100533888+geethika-1918@users.noreply.github.com>
2025-03-28 03:18:02 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
 provider "aws" {
-  region                    = "ap-south-1"
-  shared_credentials_files = ["/root/.aws/credentials"]
-  profile                  = "default"
+  region = "ap-south-1"
+}
 }
 resource "aws_instance" "kubernetes_master" {
   ami           = "ami-0e35ddab05955cf57"
