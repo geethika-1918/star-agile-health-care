543b05f2deb3b6bb82206eb090d88eb2ef5147c1
geethika-1918 <100533888+geethika-1918@users.noreply.github.com>
2025-03-28 03:53:12 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,20 +9,7 @@ terraform {
 
 provider "aws" {
   region     = "ap-south-1"
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
 }
-
-variable "aws_access_key" {
-  description = "AWS Access Key"
-  type        = string
-}
-
-variable "aws_secret_key" {
-  description = "AWS Secret Key"
-  type        = string
-}
-
 resource "aws_instance" "kubernetes_master" {
   ami           = "ami-0e35ddab05955cf57" # Ensure the AMI ID is available in your region
   instance_type = "t3.medium"
